720aa10b92f2435988f989760a2477e26322033d
gmudduluru398 <69001192+gmudduluru398@users.noreply.github.com>
2020-07-30 11:30:05 -0400

    Add files via upload
    added src files

---
+++ src/main/java/com/mastercard/codechallenge/roadconnectivity/RoadConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.mastercard.codechallenge.roadconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadConnectivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mastercard/codechallenge/roadconnectivity/RoadConnectivityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastercard.codechallenge.roadconnectivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadConnectivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
